8e648508caad58a5136248b6406e1dbff70b3097
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-09-15 15:43:27 +0100

    Rename messages to cucumber-messaged

--- java/scripts/decrypt_signing_key.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euf -o pipefail
-openssl aes-256-cbc -K $encrypted_a86bd3a8f706_key -iv $encrypted_a86bd3a8f706_iv -in scripts/codesigning.asc.enc -out scripts/codesigning.asc -d
--- java/scripts/mvn_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euf -o pipefail
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
-else
-  echo "Artifacts are only deployed on a build of the master branch"
-fi
