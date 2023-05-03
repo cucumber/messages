ae1cd045812b16cf8b5fee81dcf7b8e9c5681983
Remy Chibois <remy@chybz.net>
2023-05-03 18:12:55 +0200

    chore: compile ok

---
+++ cpp/include/cucumber/message.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <type_traits>
+#include <vector>
+
+namespace cucumber {
+
+struct message
+{
+    virtual std::string to_string() const = 0;
+};
+
+template <
+    typename Msg,
+    typename = std::enable_if_t<std::is_base_of_v<message, Msg>>
+>
+std::ostream&
+operator<<(std::ostream& os, const Msg& msg)
+{
+    os << msg.to_string();
+
+    return os;
+}
+
+template <
+    typename Msg,
+    typename = std::enable_if_t<std::is_base_of_v<message, Msg>>
+>
+std::ostream&
+operator<<(std::ostream& os, const std::vector<Msg>& msgs)
+{
+    os << '[';
+
+    for (std::size_t i = 0; i < msgs.size(); ++i) {
+        os << (i > 0 ? ", " : "") << msgs[i];
+    }
+
+    os << ']';
+
+    return os;
+}
+
+}
