19d056d04251ca45627e7897740498ff78e903b4
Luke Hill <lukehill_uk@hotmail.com>
2023-11-01 15:03:38 +0000

    Prepare release v23.0.0
    Created-by: polyglot-release v1.3.2

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -7,7 +7,7 @@ defmodule CucumberMessages.MixProject do
   def project do
     [
       app: :cucumber_messages,
-      version: "22.0.0",
+      version: "23.0.0",
       name: "CucumberMessages",
       description: description(),
       package: package(),
