b4c83aaa57afbc6a6e5c183b59c72105b28cd4e3
M.P. Korstanje <rien.korstanje@gmail.com>
2024-03-23 18:08:01 +0100

    java: include stacktrace in Convertor.toMessage(Throwable)

--- java/src/main/java/io/cucumber/messages/Convertor.java
+++ java/src/main/java/io/cucumber/messages/Convertor.java
@@ -4,29 +4,47 @@ import io.cucumber.messages.types.Duration;
 import io.cucumber.messages.types.Exception;
 import io.cucumber.messages.types.Timestamp;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static java.util.Objects.requireNonNull;
+
 public final class Convertor {
 
     private Convertor(){
 
     }
 
-    public static Exception toMessage(Throwable t) {
-        return new Exception(t.getClass().getName(), t.getMessage(), null);
+    public static Exception toMessage(Throwable throwable) {
+        requireNonNull(throwable, "throwable may not be null");
+        return new Exception(throwable.getClass().getName(), throwable.getMessage(), extractStackTrace(throwable));
+    }
+
+    private static String extractStackTrace(Throwable throwable) {
+        StringWriter stringWriter = new StringWriter();
+        try (PrintWriter printWriter = new PrintWriter(stringWriter)) {
+            throwable.printStackTrace(printWriter);
+        }
+        return stringWriter.toString();
     }
 
     public static Timestamp toMessage(java.time.Instant instant) {
+        requireNonNull(instant, "instant may not be null");
         return new Timestamp(instant.getEpochSecond(), (long) instant.getNano());
     }
 
     public static Duration toMessage(java.time.Duration duration) {
+        requireNonNull(duration, "duration may not be null");
         return new Duration(duration.getSeconds(), (long) duration.getNano());
     }
 
     public static java.time.Instant toInstant(Timestamp timestamp) {
+        requireNonNull(timestamp, "timestamp may not be null");
         return java.time.Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
     }
 
     public static java.time.Duration toDuration(Duration duration) {
+        requireNonNull(duration, "duration may not be null");
         return java.time.Duration.ofSeconds(duration.getSeconds(), duration.getNanos());
     }
 
