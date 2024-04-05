390a4bd862f5f8e75fbff8ba4c0f1d6b4a350a39
Luke Hill <lukehill_uk@hotmail.com>
2024-04-05 16:00:52 +0100

    AF: StringLiterals

--- ruby/lib/cucumber/messages/message/utils.rb
+++ ruby/lib/cucumber/messages/message/utils.rb
@@ -20,7 +20,7 @@ module Cucumber
 
             word = term.gsub(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
             word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-            word.tr!("-", "_")
+            word.tr!('-', '_')
             word.downcase!
             word
           end
