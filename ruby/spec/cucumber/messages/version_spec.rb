7cb1dd22cf656074dc455aa95d5a08ebf9379db9
Luke Hill <lukehill_uk@hotmail.com>
2024-04-05 16:09:01 +0100

    AF: RSpec/ExampleWording

--- ruby/spec/cucumber/messages/version_spec.rb
+++ ruby/spec/cucumber/messages/version_spec.rb
@@ -3,7 +3,7 @@ require 'cucumber/messages'
 module Cucumber
   module Messages
     describe VERSION do
-      it 'should be defined' do
+      it 'is defined' do
         expect(subject).to match(/\d+\.\d+\.\d+/)
       end
     end
