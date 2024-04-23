e28745ccdf950b5c7abec193cf1d0238e6185952
Luke Hill <lukehill_uk@hotmail.com>
2024-04-23 10:14:03 +0100

    Use file exist to simplify code - Remove wildcards

--- ruby/spec/cucumber/messages/acceptance_spec.rb
+++ ruby/spec/cucumber/messages/acceptance_spec.rb
@@ -5,20 +5,16 @@ require 'cucumber-compatibility-kit'
 module Cucumber
   module Messages
     describe 'messages acceptance tests' do
-      ndjson_dirs = CCK::Examples.gherkin.flat_map { |example| Dir["#{CCK::Examples.feature_code_for(example)}/**/*.ndjson"] }
-      ndjson_files = Dir["#{CCK::Examples.send(:features_folder_location)}/**/*.ndjson"]
-
       CCK::Examples.gherkin.each do |example_name|
         context "with the '#{example_name}' example" do
-          let(:example) { example_name }
-          let(:ndjson_file) { Dir["#{CCK::Examples.feature_code_for(example)}/**/*.ndjson"].first }
+          let(:ndjson_filepath) { "#{CCK::Examples.feature_code_for(example_name)}/#{example_name}.feature.ndjson" }
 
           it 'has a ndjson reference filepath' do
-            expect(ndjson_file).not_to be_empty
+            expect(File).to exist(ndjson_filepath)
           end
 
-          it "deserialises and serialises messages in the ndjson file" do
-            File.open(ndjson_file, 'r:utf-8') do |io|
+          it 'deserializes and serializes messages in the ndjson file' do
+            File.open(ndjson_filepath, 'r:utf-8') do |io|
               io.each_line do |json|
                 check(json)
               end
@@ -27,25 +23,12 @@ module Cucumber
         end
       end
 
-      # it 'must have ndjson_files as reference messages' do
-      #   expect(ndjson_dirs).not_to be_empty
-      # end
-      #
-      # ndjson_files.each do |ndjson_file|
-      #   it "deserialises and serialises messages in #{ndjson_file}" do
-      #     File.open(ndjson_file, 'r:utf-8') do |io|
-      #       io.each_line do |json|
-      #         check(json)
-      #       end
-      #     end
-      #   end
-      # end
-
       def check(json)
         hash = JSON.parse(json)
         envelope = Envelope.from_json(json)
         new_json = envelope.to_json
         new_hash = JSON.parse(new_json)
+
         expect(new_hash).to eq(hash)
       end
     end
