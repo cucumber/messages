9a967c93163e02c7615118cc2ed73719b0f1729c
Luke Hill <lukehill_uk@hotmail.com>
2024-04-25 14:20:39 +0100

    Fix up some minor simple offenses for utils spec

--- ruby/spec/cucumber/messages/message/utils_spec.rb
+++ ruby/spec/cucumber/messages/message/utils_spec.rb
@@ -1,27 +1,27 @@
 describe Cucumber::Messages::Message::Utils do
-  subject do
+  subject(:utils) do
     Class.new do
       include Cucumber::Messages::Message::Utils
     end
   end
 
   describe '#underscore' do
-    it { expect(subject.underscore('test')).to eq 'test' }
-    it { expect(subject.underscore('testTest')).to eq 'test_test' }
-    it { expect(subject.underscore('')).to eq '' }
-    it { expect(subject.underscore('T')).to eq 't' }
-    it { expect(subject.underscore('test123test456Test')).to eq 'test123test456_test' }
-    it { expect(subject.underscore('test-test')).to eq 'test_test' }
-    it { expect(subject.underscore('TEST_Test')).to eq 'test_test' }
-    it { expect(subject.underscore('test-Test')).to eq 'test_test' }
+    it { expect(utils.underscore('test')).to eq('test') }
+    it { expect(utils.underscore('testTest')).to eq('test_test') }
+    it { expect(utils.underscore('')).to eq('') }
+    it { expect(utils.underscore('T')).to eq('t') }
+    it { expect(utils.underscore('test123test456Test')).to eq('test123test456_test') }
+    it { expect(utils.underscore('test-test')).to eq('test_test') }
+    it { expect(utils.underscore('TEST_Test')).to eq('test_test') }
+    it { expect(utils.underscore('test-Test')).to eq('test_test') }
   end
 
   describe '#camelize' do
-    it { expect(subject.camelize('test')).to eq 'test' }
-    it { expect(subject.camelize('test_test')).to eq 'testTest' }
-    it { expect(subject.camelize('Test_TeSt')).to eq 'TestTest' }
-    it { expect(subject.camelize('')).to eq '' }
-    it { expect(subject.camelize('test123test4_5_6_test')).to eq 'test123test456Test' }
-    it { expect(subject.camelize('test-test')).to eq 'test-test' }
+    it { expect(utils.camelize('test')).to eq('test') }
+    it { expect(utils.camelize('test_test')).to eq('testTest') }
+    it { expect(utils.camelize('Test_TeSt')).to eq('TestTest') }
+    it { expect(utils.camelize('')).to eq('') }
+    it { expect(utils.camelize('test123test4_5_6_test')).to eq('test123test456Test') }
+    it { expect(utils.camelize('test-test')).to eq('test-test') }
   end
 end
