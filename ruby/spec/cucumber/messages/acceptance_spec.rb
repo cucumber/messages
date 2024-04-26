0d80d4deda10c506d45202688b4eb0a788593454
Luke Hill <lukehill_uk@hotmail.com>
2024-04-26 11:10:09 +0100

    Attempt to distinguish what are the actual objects in the acceptance tests

--- ruby/spec/cucumber/messages/acceptance_spec.rb
+++ ruby/spec/cucumber/messages/acceptance_spec.rb
@@ -13,22 +13,17 @@ module Cucumber
           end
 
           it 'deserializes and serializes messages in the ndjson file' do
-            File.open(ndjson_filepath, 'r:utf-8') do |io|
-              io.each_line do |json|
-                check(json)
+            File.open(ndjson_filepath, 'r:utf-8') do |file|
+              file.each_line do |message|
+                parsed_actual_message = JSON.parse(message)
+                generated_message = Envelope.from_json(message).to_json
+                parsed_regenerated_message = JSON.parse(generated_message)
+
+                expect(parsed_regenerated_message).to eq(parsed_actual_message)
               end
             end
           end
         end
-      end
-
-      def check(json)
-        hash = JSON.parse(json)
-        envelope = Envelope.from_json(json)
-        new_json = envelope.to_json
-        new_hash = JSON.parse(new_json)
-
-        expect(new_hash).to eq(hash)
       end
     end
   end
