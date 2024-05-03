c713902f295f5900af2a3fc827f90ffcbf63ebe7
Luke Hill <lukehill_uk@hotmail.com>
2024-05-03 14:40:19 +0100

    Simplify deserialization protocol into just a single class method which is extended

--- ruby/lib/cucumber/messages/message.rb
+++ ruby/lib/cucumber/messages/message.rb
@@ -4,7 +4,7 @@ require 'cucumber/messages/message/serialization'
 module Cucumber
   module Messages
     class Message
-      include Cucumber::Messages::Message::Deserialization
+      extend Cucumber::Messages::Message::Deserialization
       include Cucumber::Messages::Message::Serialization
     end
   end
