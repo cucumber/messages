278e62bd901baab6545165599ef65d666c412091
Luke Hill <lukehill_uk@hotmail.com>
2024-05-07 14:03:07 +0100

    Alter UUID test to just check for a securerandom uuid and simplify the code

---
+++ ruby/spec/cucumber/messages/id_generator/uuid_spec.rb
@@ -0,0 +1,11 @@
+describe Cucumber::Messages::IdGenerator::UUID do
+  subject(:generator) { described_class.new }
+
+  describe '#new_id' do
+    it 'generates a new UUID' do
+      expect(SecureRandom).to receive(:uuid)
+
+      generator.new_id
+    end
+  end
+end
