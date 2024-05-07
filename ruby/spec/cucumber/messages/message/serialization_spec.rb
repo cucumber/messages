abed109bd89c9b68e8f27f76ef3c3bc6c69aaf56
Luke Hill <lukehill_uk@hotmail.com>
2024-05-07 11:45:46 +0100

    Simplify serialization spec by not creating 10 different subjects

--- ruby/spec/cucumber/messages/message/serialization_spec.rb
+++ ruby/spec/cucumber/messages/message/serialization_spec.rb
@@ -1,63 +1,55 @@
 describe Cucumber::Messages::Message::Serialization do
   context 'with a simple message' do
-    describe '#to_h' do
-      subject(:message) { Cucumber::Messages::SimpleMessage.new.to_h }
+    subject(:message) { Cucumber::Messages::SimpleMessage.new }
 
+    describe '#to_h' do
       it 'returns a Hash with all the attributes of the Message' do
-        expect(message).to eq({ is_nil: nil, is_string: '', is_array: [], is_number: 0 })
+        expect(message.to_h).to eq({ is_nil: nil, is_string: '', is_array: [], is_number: 0 })
       end
 
       context 'with camelize: true' do
-        subject(:message) { Cucumber::Messages::SimpleMessage.new.to_h(camelize: true) }
-
         it 'camelizes the keys of the resulting hash' do
-          expect(message).to eq({ isNil: nil, isString: '', isArray: [], isNumber: 0 })
+          expect(message.to_h(camelize: true)).to eq({ isNil: nil, isString: '', isArray: [], isNumber: 0 })
         end
       end
 
       context 'with reject_nil_values: true' do
-        subject(:message) { Cucumber::Messages::SimpleMessage.new.to_h(reject_nil_values: true) }
-
         it 'rejects nil values from the resulting hash' do
-          expect(message).to eq({ is_string: '', is_array: [], is_number: 0 })
+          expect(message.to_h(reject_nil_values: true)).to eq({ is_string: '', is_array: [], is_number: 0 })
         end
       end
     end
 
     describe '#to_json' do
-      subject(:message) { Cucumber::Messages::SimpleMessage.new.to_json }
-
       it 'returns a JSON document with all the attributes of the Message except nil values' do
-        expect(message).to eq({ isString: '', isArray: [], isNumber: 0 }.to_json)
+        expect(message.to_json).to eq({ isString: '', isArray: [], isNumber: 0 }.to_json)
       end
     end
   end
 
   context 'with a message which embeds other messages' do
-    describe '#to_h' do
-      subject(:message) { Cucumber::Messages::ComprehensiveMessage.new.to_h }
+    subject(:message) { Cucumber::Messages::ComprehensiveMessage.new }
 
+    describe '#to_h' do
       it 'includes a hash representation of embedded messages' do
-        expect(subject[:simple_message]).to eq({ is_nil: nil, is_string: '', is_array: [], is_number: 0 })
-        expect(subject[:is_enum]).to eq('an enum')
+        expect(message.to_h[:simple_message]).to eq({ is_nil: nil, is_string: '', is_array: [], is_number: 0 })
+        expect(message.to_h[:is_enum]).to eq('an enum')
       end
 
       it 'includes a hash representation of messages arrays' do
-        expect(subject[:message_array]).to eq([
+        expect(message.to_h[:message_array]).to eq([
           { is_nil: nil, is_string: '', is_array: [], is_number: 0 },
           { is_nil: nil, is_string: '', is_array: [], is_number: 0 }
         ])
       end
 
       context 'with camelize: true' do
-        subject(:message) { Cucumber::Messages::ComprehensiveMessage.new.to_h(camelize: true) }
-
         it 'camelizes the keys of the embedded messages resulting hashes' do
-          expect(subject[:simpleMessage]).to eq({ isNil: nil, isString: '', isArray: [], isNumber: 0 })
+          expect(message.to_h(camelize: true)[:simpleMessage]).to eq({ isNil: nil, isString: '', isArray: [], isNumber: 0 })
         end
 
         it 'camelizes the keys of hashes for messages arrays' do
-          expect(subject[:messageArray]).to eq([
+          expect(message.to_h(camelize: true)[:messageArray]).to eq([
             { isNil: nil, isString: '', isArray: [], isNumber: 0 },
             { isNil: nil, isString: '', isArray: [], isNumber: 0 }
           ])
@@ -66,15 +58,13 @@ describe Cucumber::Messages::Message::Serialization do
     end
 
     describe '#to_json' do
-      subject(:message) { Cucumber::Messages::ComprehensiveMessage.new.to_json }
-
       it 'returns a JSON document with embedded messages' do
-        expect(message).to include({ isString: '', isArray: [], isNumber: 0 }.to_json)
-        expect(message).to include('"isEnum":"an enum"')
+        expect(message.to_json).to include({ isString: '', isArray: [], isNumber: 0 }.to_json)
+        expect(message.to_json).to include('"isEnum":"an enum"')
       end
 
       it 'returns a JSON document with messages arrays' do
-        expect(message).to include(
+        expect(message.to_json).to include(
           [
             { isString: '', isArray: [], isNumber: 0 },
             { isString: '', isArray: [], isNumber: 0 }
