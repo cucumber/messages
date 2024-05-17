a7b32b0c1dd56a6deb809e90a554250d1f46c0cf
Luke Hill <lukehill_uk@hotmail.com>
2024-05-17 15:38:06 +0100

    Move some logic out into let/subject blocks

--- ruby/spec/cucumber/messages/ndjson_to_message_enumerator_spec.rb
+++ ruby/spec/cucumber/messages/ndjson_to_message_enumerator_spec.rb
@@ -1,42 +1,38 @@
 # frozen_string_literal: true
 
 describe Cucumber::Messages::NdjsonToMessageEnumerator do
+  subject(:incoming_messages) { Cucumber::Messages::NdjsonToMessageEnumerator.new(io) }
+
   let(:outgoing_messages) do
     [
-      Cucumber::Messages::Envelope.new(source: Cucumber::Messages::Source.new(data: 'Feature: Hello')),
-      Cucumber::Messages::Envelope.new(attachment: Cucumber::Messages::Attachment.new(body: 'Hello', content_encoding: Cucumber::Messages::AttachmentContentEncoding::IDENTITY))
+      Cucumber::Messages::Envelope.new(attachment: attachment),
+      Cucumber::Messages::Envelope.new(source: source)
     ]
   end
+  let(:attachment) { Cucumber::Messages::Attachment.new(body: 'Hello', content_encoding: Cucumber::Messages::AttachmentContentEncoding::IDENTITY) }
+  let(:source) { Cucumber::Messages::Source.new(data: 'Feature: Hello') }
+  let(:io) { StringIO.new }
 
   it 'can be serialised over an ndjson stream' do
-    io = StringIO.new
     write_outgoing_messages(outgoing_messages, io)
-
     io.rewind
-    incoming_messages = Cucumber::Messages::NdjsonToMessageEnumerator.new(io)
 
-    expect(incoming_messages.to_a.map(&:to_h)).to(eq(outgoing_messages.map(&:to_h)))
+    expect(incoming_messages.to_a.map(&:to_h)).to eq(outgoing_messages.map(&:to_h))
   end
 
   it 'ignores empty lines' do
-    io = StringIO.new
     write_outgoing_messages(outgoing_messages, io)
     io.write("\n\n")
-
     io.rewind
-    incoming_messages = Cucumber::Messages::NdjsonToMessageEnumerator.new(io)
 
-    expect(incoming_messages.to_a.map(&:to_h)).to(eq(outgoing_messages.map(&:to_h)))
+    expect(incoming_messages.to_a.map(&:to_h)).to eq(outgoing_messages.map(&:to_h))
   end
 
   it 'includes offending line in error message' do
-    io = StringIO.new
     io.puts('BLA BLA')
-
     io.rewind
-    incoming_messages = Cucumber::Messages::NdjsonToMessageEnumerator.new(io)
 
-    expect { incoming_messages.to_a }.to(raise_error('Not JSON: BLA BLA'))
+    expect { incoming_messages.to_a }.to raise_error('Not JSON: BLA BLA')
   end
 
   def write_outgoing_messages(messages, out)
