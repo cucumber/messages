65bf78f75323e6d14582727b9515ffa3afd801ce
Luke Hill <lukehill_uk@hotmail.com>
2024-06-14 15:48:47 +0100

    Improve perl complexity/length of methods in generators

--- codegen/codegen/perl.rb
+++ codegen/codegen/perl.rb
@@ -36,18 +36,22 @@ module Codegen
 
     def default_value(parent_type_name, property_name, property)
       if property['type'] == 'string'
-        if property['enum']
-          "#{property_name.upcase}_#{enum_constant(property['enum'][0])}"
-        else
-          "''"
-        end
+        default_value_for_string(property_name, property['enum'])
       elsif property['type'] == 'boolean'
-        "''" # an empty string renders evaluates to false
+        "''" # an empty string rendering will evaluate to false - this won't
       elsif property['$ref']
         type = type_for(parent_type_name, nil, property)
         "#{type}->new()"
       else
         super(parent_type_name, property_name, property)
+      end
+    end
+
+    def default_value_for_string(property_name, enum)
+      if enum
+        "#{property_name.upcase}_#{enum_constant(enum[0])}"
+      else
+        "''" # an empty string rendering will evaluate to false - this won't
       end
     end
   end
