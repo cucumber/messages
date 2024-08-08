839ee14bd3f7528f56233bdbac6ec4ac5e70e49a
Luke Hill <lukehill_uk@hotmail.com>
2024-08-08 13:41:11 +0100

    Namespace the dotnet generator and remove the initializer override

--- codegen/generators/dotnet.rb
+++ codegen/generators/dotnet.rb
@@ -1,26 +1,30 @@
-class Dotnet < Codegen
-  def initialize(paths)
-    language_type_by_schema_type = {
-      'integer' => 'long',
-      'string' => 'string',
-      'boolean' => 'bool',
-    }
+module Generator
+  # Generator::Dotnet
+  # Automatic Code generation overrides for the .NET programming language
+  class Dotnet < Base
+    def array_type_for(type_name)
+      "List<#{type_name}>"
+    end
 
-    super(paths, language_type_by_schema_type)
-  end
+    def format_description(raw_description, indent_string: '')
+      return '' if raw_description.nil?
 
-  def array_type_for(type_name)
-    "List<#{type_name}>"
-  end
+      raw_description
+        .split("\n")
+        .map { |line| line.strip }
+        .filter { |line| line != '*' }
+        .map { |line| " * #{line}".rstrip }
+        .join("\n#{indent_string}")
+    end
 
-  def format_description(raw_description, indent_string: '')
-    return '' if raw_description.nil?
+    private
 
-    raw_description
-      .split("\n")
-      .map { |line| line.strip }
-      .filter { |line| line != '*' }
-      .map { |line| " * #{line}".rstrip() }
-      .join("\n#{indent_string}")
+    def language_translations_for_data_types
+      {
+        'integer' => 'long',
+        'string' => 'string',
+        'boolean' => 'bool'
+      }
+    end
   end
 end
