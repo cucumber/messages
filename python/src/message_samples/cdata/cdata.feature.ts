118039e38e025359c9231558214096a9457bb1af
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2024-09-05 01:05:48 +0300

    Bare minimum tests for python model

--- python/src/message_samples/cdata/cdata.feature.ts
+++
@@ -1,6 +0,0 @@
-import assert from 'assert'
-import { Given } from '@cucumber/fake-cucumber'
-
-Given('I have {int} <![CDATA[cukes]]> in my belly', function (cukeCount: number) {
-  assert(cukeCount)
-})
--- python/tests/test_model_load.py
+++ python/tests/test_model_load.py
@@ -1,52 +1,28 @@
 import json
-import re
 from pathlib import Path
 
-from git import Repo
-from packaging import version
-from pytest import fixture
+from pytest import mark, param
 
 from cucumber_messages import Envelope
 
-
-@fixture
-def compatibility_kit_repo(tmpdir):
-    repo_path = Path(tmpdir) / "compatibility-kit"
-    repo = Repo.clone_from(
-        "https://github.com/cucumber/compatibility-kit.git",
-        str(repo_path),
-        branch="main",
-    )
-    repo_tags = list(filter(lambda tag: tag is not None, map(lambda tag: getattr(tag.tag, "tag", None), repo.tags)))
-
-    version_pattern = re.compile(r"((.*/)?)v(\d+\.\d+\.\d+)")
-    last_version = sorted(
-        map(
-            version.parse,
-            map(
-                lambda match: match.groups()[-1],
-                filter(lambda match: match is not None, map(lambda tag: re.match(version_pattern, tag), repo_tags)),
-            ),
-        )
-    )[-1]
-
-    last_version_tag = next(filter(lambda tag: re.search(re.escape(str(last_version)), tag), repo_tags))
-
-    repo.git.checkout(last_version_tag)
-
-    return repo_path
+resource_path = Path(__file__).parent.absolute()
 
 
-def test_simple_load_model(compatibility_kit_repo):
-    for ast_path in (compatibility_kit_repo / "devkit" / "samples").rglob("*.ndjson"):
-        print(f"Checking ${ast_path}")
-        with ast_path.open(mode="r") as ast_file:
-            for ast_line in ast_file:
-                model_datum = json.loads(ast_line)
-                model = Envelope.model_validate(model_datum)  # type: ignore[attr-defined]
+@mark.parametrize(
+    "ast_path",
+    map(
+        lambda file: param(file, id=file.name),  # type: ignore[no-any-return]
+        resource_path.rglob("*.ndjson"),
+    ),
+)
+def test_simple_load_model(ast_path: Path):
+    with ast_path.open(mode="r") as ast_file:
+        for ast_line in ast_file:
+            model_datum = json.loads(ast_line)
+            model = Envelope.model_validate(model_datum)  # type: ignore[attr-defined]
 
-                assert isinstance(model, Envelope)
+            assert isinstance(model, Envelope)
 
-                dumped_ast_datum = json.loads(model.model_dump_json(by_alias=True, exclude_none=True))  # type: ignore[attr-defined] # migration to pydantic2
+            dumped_ast_datum = json.loads(model.model_dump_json(by_alias=True, exclude_none=True))
 
-                assert model_datum == dumped_ast_datum
+            assert model_datum == dumped_ast_datum
