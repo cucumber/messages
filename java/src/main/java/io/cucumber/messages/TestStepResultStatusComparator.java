4d1f03648696e035e9bc7488ad306ccea206eb4d
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2025-07-21 16:37:01 +0200

    java: Add comparators for comparable objects (#312)
    Duration, Location and timestamp are comparable. To help sorting these
    messages a comparator is needed. The alternative would be implementing
    the `Comparable` interface, but with generated code that is quite
    tricky.

---
+++ java/src/main/java/io/cucumber/messages/TestStepResultStatusComparator.java
@@ -0,0 +1,15 @@
+package io.cucumber.messages;
+
+import io.cucumber.messages.types.TestStepResultStatus;
+
+import java.util.Comparator;
+
+/**
+ * Orders test step results from least to most severe.
+ */
+public final class TestStepResultStatusComparator implements Comparator<TestStepResultStatus> {
+    @Override
+    public int compare(TestStepResultStatus a, TestStepResultStatus b) {
+        return Integer.compare(a.ordinal(), b.ordinal());
+    }
+}
