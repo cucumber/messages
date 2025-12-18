a7abd6128f0469e6619fd026abb3aafccc411d2d
M.P. Korstanje <rien.korstanje@gmail.com>
2025-12-18 16:21:12 +0100

    Make Ruby pass

--- ruby/spec/cucumber/messages/acceptance_spec.rb
+++ ruby/spec/cucumber/messages/acceptance_spec.rb
@@ -1,20 +1,15 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'cucumber-compatibility-kit'
 
 module Cucumber
   module Messages
     describe 'messages acceptance tests' do
-      CCK::Examples.gherkin.each do |example_name|
-        context "with the '#{example_name}' example" do
-          let(:ndjson_filepath) { "??.ndjson" }
-
-          it 'has a ndjson reference filepath' do
-            expect(File).to exist(ndjson_filepath)
-          end
-
+      Dir.entries('../testdata').filter { |file| File.extname(file) == ".ndjson" }.each do |example| 
+        context "with the '#{example}'" do
           it 'deserializes and serializes messages in the ndjson file' do
-            File.open(ndjson_filepath, 'r:utf-8') do |file|
+            File.open("../testdata/#{example}", 'r:utf-8') do |file|
               file.each_line do |message|
                 # Check that the Envelope re-generated message equals the originally parsed message
                 expect(JSON.parse(Envelope.from_json(message).to_json)).to eq(JSON.parse(message))
