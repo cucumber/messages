7950d12d60b62980a0e77d29951836092984243b
M.P. Korstanje <rien.korstanje@gmail.com>
2025-12-18 16:28:54 +0100

    Fix python

--- python/tests/test_model_load.py
+++ python/tests/test_model_load.py
@@ -1,17 +1,12 @@
 import json
-import re
 from pathlib import Path
-
-from git import Repo
-from packaging import version
-from pytest import fixture
 
 from cucumber_messages import Envelope, message_converter
 
 
 # Analog of "ruby/spec/cucumber/messages/acceptance_spec.rb" test
-def test_simple_load_model(compatibility_kit_repo):
-    for ast_path in (/".." / "testdata" ).rglob("*.ndjson"):
+def test_simple_load_model():
+    for ast_path in (Path(__file__).resolve().parent / "testdata").rglob("*.ndjson"):
         print(f"Checking ${ast_path}")
         with ast_path.open(mode="r") as ast_file:
             for ast_line in ast_file:
