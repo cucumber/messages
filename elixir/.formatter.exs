bb0f13e255505ef6591bec044d2811ab2be12f80
M.P. Korstanje <rien.korstanje@gmail.com>
2026-06-07 21:43:01 +0200

    Exclude generated code from formatting

--- elixir/.formatter.exs
+++ elixir/.formatter.exs
@@ -1,3 +1,6 @@
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: [
+    "{mix,.formatter}.exs",
+    "{config,test}/**/*.{ex,exs}, lib/cucumber_messages/*.{ex,exs}"
+  ]
 ]
