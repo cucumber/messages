cccf36df16a454052e6ff0430d3be5f934bb6563
M.P. Korstanje <rien.korstanje@gmail.com>
2026-06-23 02:12:19 +0200

    Replace com.fasterxml.jackson with tools.jackson

--- java/src/test/java/io/cucumber/messages/JacksonTest.java
+++ java/src/test/java/io/cucumber/messages/JacksonTest.java
@@ -1,6 +1,5 @@
 package io.cucumber.messages;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.cucumber.messages.types.Envelope;
 import io.cucumber.messages.types.Source;
 import io.cucumber.messages.types.TestRunStarted;
@@ -15,20 +14,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JacksonTest {
     @Test
-    void can_deserialize_enum() throws JsonProcessingException {
+    void can_deserialize_enum() {
         Source source = new Source("hello.feature", "Feature: Hello", TEXT_X_CUCUMBER_GHERKIN_PLAIN);
         String json = OBJECT_MAPPER.writeValueAsString(source);
         assertEquals(source, OBJECT_MAPPER.readValue(json, Source.class));
     }
 
     @Test
-    void serialize_enums_using_value() throws JsonProcessingException {
+    void serialize_enums_using_value() {
         assertEquals("\"text/x.cucumber.gherkin+plain\"",
                 OBJECT_MAPPER.writeValueAsString(TEXT_X_CUCUMBER_GHERKIN_PLAIN));
     }
 
     @Test
-    void can_deserialize_envelope() throws JsonProcessingException {
+    void can_deserialize_envelope() {
         Envelope source = Envelope.of(new TestRunStarted(new Timestamp(3L, 14), UUID.randomUUID().toString()));
         String json = OBJECT_MAPPER.writeValueAsString(source);
         assertEquals(source, OBJECT_MAPPER.readValue(json, Envelope.class));
