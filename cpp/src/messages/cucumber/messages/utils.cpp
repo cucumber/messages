4b6b2f6ee16f8adda37e44f070d78a9169b647f7
Timmer, Daan <daan.timmer@philips.com>
2026-06-30 15:11:27 +0000

    refactored utils.hpp to_string to remove complexity

--- cpp/src/messages/cucumber/messages/utils.cpp
+++ cpp/src/messages/cucumber/messages/utils.cpp
@@ -1,28 +1,35 @@
-#include <cucumber/messages/utils.hpp>
+#include "cucumber/messages/utils.hpp"
+#include <cctype>
+#include <cstddef>
+#include <string>
 
-namespace cucumber::messages {
-
-// https://codereview.stackexchange.com/questions/263749/codewars-kata-converting-snake-case-identifiers-to-camelcase-in-c
-std::string
-camelize(std::string s, bool initial_tail)
+namespace cucumber::messages
 {
-    std::size_t n = 0;
-    bool tail = initial_tail;
+    // https://codereview.stackexchange.com/questions/263749/codewars-kata-converting-snake-case-identifiers-to-camelcase-in-c
+    std::string camelize(std::string str, bool initial_tail)
+    {
+        std::size_t writeIndex = 0;
+        bool tail = initial_tail;
 
-    for (unsigned char c : s) {
-        if (c == '-' || c == '_') {
-            tail = false;
-        } else if (tail) {
-            s[n++] = c;
-        } else {
-            tail = true;
-            s[n++] = std::toupper(c);
+        for (const auto chr : str)
+        {
+            if (chr == '-' || chr == '_')
+            {
+                tail = false;
+            }
+            else if (tail)
+            {
+                str[writeIndex++] = chr;
+            }
+            else
+            {
+                tail = true;
+                str[writeIndex++] = static_cast<char>(std::toupper(chr));
+            }
         }
+
+        str.resize(writeIndex);
+
+        return str;
     }
-
-    s.resize(n);
-
-    return s;
-}
-
 }
--- cpp/src/messages/cucumber/messages/utils.hpp
+++ cpp/src/messages/cucumber/messages/utils.hpp
@@ -1,93 +1,48 @@
-#pragma once
-// clang-format off
-#include "nlohmann/json_fwd.hpp"
+#ifndef CUCUMBER_MESSAGES_UTILS_HPP
+#define CUCUMBER_MESSAGES_UTILS_HPP
+
 #include "nlohmann/json.hpp"
-#include <memory>
+#include "nlohmann/json_fwd.hpp"
+#include <optional>
 #include <ostream>
-#include <sstream>
 #include <string>
 #include <string_view>
-#include <utility>
 #include <vector>
-#include <optional>
-#include <type_traits>
 
-
-namespace cucumber::messages {
-
-using json = nlohmann::json;
-
-template <
-    typename T,
-    template <typename...> typename Primary
->
-struct is_specialization_of : std::false_type {};
-
-template <
-    template <typename...> typename Primary,
-    typename... Args
->
-struct is_specialization_of<Primary<Args...>, Primary> : std::true_type {};
-
-template <
-    typename T,
-    template <typename...> class Primary
->
-using
-is_specialization_of_t = is_specialization_of<T, Primary>;
-
-template <
-    typename T,
-    template <typename...> class Primary
->
-inline
-constexpr bool
-is_specialization_of_v = is_specialization_of_t<T, Primary>::value;
-
-template <
-    typename T,
-    typename Callable
->
-void
-apply_if(const T& v, Callable&& cb)
+namespace cucumber::messages
 {
-    if constexpr (is_specialization_of_v<T, std::optional>) {
-        if (v) {
-            cb(*v);
-        }
-    } else {
-        cb(v);
+    std::string camelize(std::string str, bool initial_tail = true);
+
+    template<typename T>
+    void to_string(std::ostream& ostream, std::string_view prefix, const T& value)
+    {
+        ostream << prefix << value;
     }
-}
 
-std::string
-camelize(std::string s, bool initial_tail = true);
+    template<typename T>
+    void to_string(std::ostream& ostream, std::string_view prefix, const std::vector<T>& vector)
+    {
+        ostream << prefix << '[';
 
-template <typename P, typename T>
-void
-to_string(std::ostream& os, P&& prefix, T&& opt)
-{
-    apply_if(
-        opt,
-        [&](const auto& v) {
-            using vtype = std::decay_t<decltype(v)>;
-
-            if constexpr (is_specialization_of_v<vtype, std::vector>) {
-                os << prefix << '[';
-
-                for (std::size_t i = 0; i < v.size(); ++i) {
-                    os << (i > 0 ? ", " : "") << v[i];
-                }
-
-                os << ']';
-            } else {
-                os << prefix << v;
+        if (!vector.empty())
+        {
+            ostream << vector.front();
+            for (auto iter = std::next(vector.begin()); iter != vector.end(); ++iter)
+            {
+                ostream << ", " << *iter;
             }
         }
-    );
-}
+        ostream << ']';
+    }
 
-    // clang-format on
+    template<typename T>
+    void to_string(std::ostream& ostream, std::string_view prefix, const std::optional<T>& optional)
+    {
+        if (optional.has_value())
+        {
+            to_string(ostream, prefix, *optional);
+        }
+    }
 
     template<typename T>
     void to_json(nlohmann::json& json, std::string_view key, const T& opt)
@@ -107,7 +62,6 @@ to_string(std::ostream& os, P&& prefix, T&& opt)
     template<typename T>
     void from_json(const nlohmann::json& json, std::string_view key, T& opt)
     {
-
         json.at(key).get_to(opt);
     }
 
@@ -120,3 +74,5 @@ to_string(std::ostream& os, P&& prefix, T&& opt)
         }
     }
 }
+
+#endif
