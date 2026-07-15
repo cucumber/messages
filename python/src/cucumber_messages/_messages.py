91d3f6a108d6c29e5c1745063c8693eb43d29fc5
Luke Hill <lukehill_uk@hotmail.com>
2026-07-15 10:49:47 +0100

    Regen with new docs

--- python/src/cucumber_messages/_messages.py
+++ python/src/cucumber_messages/_messages.py
@@ -477,7 +477,7 @@ class PickleStepArgument:
 @dataclass
 class PickleTable:
     rows: list[PickleTableRow]
-    argument_index: Optional[int] = None  # The index of this argument. The value is 0 if it was declared before the doc string, 1 if it was declared after.
+    argument_index: Optional[int] = None  # The index of this argument. The value is 1 if it was declared before the doc string, 2 if it was declared after.
 
 
 @dataclass
