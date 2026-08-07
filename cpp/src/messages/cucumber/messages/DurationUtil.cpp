e13c029f4bd74cb2d92278b935c3e861e47ea5a4
Timmer, Daan <daan.timmer+github@gmail.com>
2026-08-07 13:00:24 +0000

    Implement DurationUtil for timestamp operations and add corresponding tests

---
+++ cpp/src/messages/cucumber/messages/DurationUtil.cpp
@@ -0,0 +1,35 @@
+#include "cucumber/messages/DurationUtil.hpp"
+#include "cucumber/messages/Duration.hpp"
+#include "cucumber/messages/Timestamp.hpp"
+#include <chrono>
+#include <ctime>
+
+namespace cucumber::messages
+{
+    namespace
+    {
+        std::chrono::nanoseconds ToNanos(std::chrono::seconds seconds, std::chrono::nanoseconds nanos)
+        {
+            return std::chrono::duration_cast<std::chrono::nanoseconds>(seconds) + nanos;
+        }
+
+        std::chrono::nanoseconds TimestampToNanos(const Timestamp& timestamp)
+        {
+            return ToNanos(std::chrono::seconds(timestamp.seconds), std::chrono::nanoseconds(timestamp.nanos));
+        }
+
+        Duration ToDuration(std::chrono::nanoseconds millis)
+        {
+            return {
+                .seconds = millis.count() / nanosecondsPerSecond,
+                .nanos = millis.count() % nanosecondsPerSecond,
+            };
+        }
+    }
+
+    Duration operator-(const Timestamp& lhs, const Timestamp& rhs)
+    {
+        const auto durationMillis = TimestampToNanos(lhs) - TimestampToNanos(rhs);
+        return ToDuration(durationMillis);
+    }
+}
