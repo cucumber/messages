5774db48faa42809c099c4a3138425708dda0dbe
Andrew Friedman <andrewfriedman01@gmail.com>
2026-08-31 19:22:53 -0400

    feat(main): replace print with stdout.writeln for better output handling

--- dart/example/main.dart
+++ dart/example/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cucumber_messages/cucumber_messages.dart';
 
 Future<void> main() async {
@@ -12,6 +14,6 @@ Future<void> main() async {
   final lines = encodeNdjsonEnvelopes(Stream.value(envelope));
 
   await for (final decoded in decodeNdjsonEnvelopes(lines)) {
-    print(decoded.attachment?.body);
+    stdout.writeln(decoded.attachment?.body);
   }
 }
